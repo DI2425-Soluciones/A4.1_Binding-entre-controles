f8004d8f165606ec7d2782d6d7e49145fd5ab097
Madrigal <Madrigal@A208PC03>
2024-11-18 11:46:15 +0100

    Agregar archivos de proyecto.

---
+++ MemeMaker/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MemeMaker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
